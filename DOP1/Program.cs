f4d5b36d0ece71671597aee80a52032410615094
anwind-git <work-git@yandex.ru>
2022-10-04 19:12:40 +0300

    перевод десятичного числа в двоичное, используя рекурсию.

---
+++ DOP1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать перевод десятичного числа в двоичное, используя рекурсию.
+
+void Rev(int number = 156, string result = "")
+{
+    if(number == 0) return;
+    result = number % 2 + result;
+    number = number / 2; 
+    Rev(number);
+    Console.Write(result);
+}
+Rev();
