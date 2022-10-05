936ae5b9f5b5f70db3b7648d380ca0596cae4395
anwind-git <work-git@yandex.ru>
2022-10-05 12:31:51 +0300

    Используя рекурсию, подсчитайте, сколько в поговорке гласных букв.

---
+++ DOP2/Program.cs
@@ -0,0 +1,15 @@
+﻿// На вход подаётся поговорка “без труда не выловишь и рыбку из пруда”.
+// Используя рекурсию, подсчитайте, сколько в поговорке гласных букв.
+
+void Podshet(int i = 0, string text = "без труда не выловишь и рыбку из пруда")
+{
+    if(i > 0 && i < text.Length) return;
+       text = text.ToLower();
+       char[] glas = {'а', 'е', 'ё', 'у', 'ы', 'я', 'и', 'о', 'ю', 'э'};
+       foreach (char d in text) 
+            if (glas.Contains(d))
+                i++;
+    Console.WriteLine($"Гласных букв в строке: " + i);
+    Podshet(i);
+}
+Podshet();
